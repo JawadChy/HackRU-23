6768d0298aa7eff4911606445d398f557b202c67
Jerrely Vizueta <victorv2718@gmail.com>
2023-10-16 20:30:20 -0400

    Corrected bus stops API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./App.css";
+import axios from 'axios'
 import ComboBox from "./components/ComboBox";
+
 
 function App() {
   const [route, setRoute] = useState(null);
   const [from, setFrom] = useState("");
   const [to, setTo] = useState("");
+  const [bus_stops, setStop] = useState('');
   const navigate = useNavigate();
+  
+  useEffect(() => {
+    axios.get('http://localhost:3000/bus_stops')
+      .then((response) => {
+        console.log(response);
+        setStop(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
   const handleRouteChange = (event, newInputValue) => {
     setRoute(newInputValue);
@@ -45,86 +59,86 @@ function App() {
     }
   };
 
-  const bus_stops = {
-    508: [
-      { id: 1, label: "ACROSS ATLANTIC AVE AT OHIO AVE" },
-      { id: 2, label: "ALBANY AVE AT CROSSAN AVE" },
-      { id: 3, label: "ALBANY AVE AT FILBERT AVE" },
-      { id: 4, label: "ATLANTIC AVE AT ARKANSAS AVE" },
-      { id: 5, label: "ATLANTIC AVE AT BOSTON AVE" },
-      { id: 6, label: "ATLANTIC AVE AT BRIGHTON AVE" },
-      { id: 7, label: "ATLANTIC AVE AT CALIFORNIA AVE" },
-      { id: 8, label: "ATLANTIC AVE AT CHELSEA AVE" },
-      { id: 9, label: "ATLANTIC AVE AT FLORIDA AVE" },
-      { id: 10, label: "ATLANTIC AVE AT KENTUCKY AVE" },
-      { id: 11, label: "ATLANTIC AVE AT MARTIN LUTHER KING BLVD" },
-      { id: 12, label: "ATLANTIC AVE AT MISSISSIPPI AVE" },
-      { id: 13, label: "ATLANTIC AVE AT MONTPELIER AVE" },
-      { id: 14, label: "ATLANTIC AVE AT NEW YORK AVE" },
-      { id: 15, label: "ATLANTIC CARE ACCESS RD AT HACKBERRY LANE" },
-      { id: 16, label: "ATLANTIC COUNTY - SOCIAL SECURITY OFFICE" },
-      { id: 17, label: "ATLANTICARE REGIONAL MEDICAL CENTER" },
-      { id: 18, label: "CALIFORNIA AVE AT FALCON DR" },
-      { id: 19, label: "CALIFORNIA AVE AT NEW RD" },
-      { id: 20, label: "COLLINS RD AT FEDERAL CT" },
-      { id: 21, label: "COLLINS RD AT GREENWICH DR. EAST" },
-      { id: 22, label: "FIRE RD AT CALIFORNIA AVE. (2511 FIRE)#" },
-      { id: 23, label: "HAMILTON MALL" },
-      { id: 24, label: "JIMMIE LEEDS RD AT ASH AVE" },
-      { id: 25, label: "JIMMIE LEEDS RD AT PITNEY RD" },
-      { id: 26, label: "JIMMIE LEEDS RD AT PITNEY RD#" },
-      { id: 27, label: "JIMMIE LEEDS RD AT SEASHORE GARDENS ENTRANCE" },
-      { id: 28, label: "JIMMIE LEEDS RD AT SEAVIEW AVE" },
-      { id: 29, label: "MAIN ST AT ADAMS AVE" },
-      { id: 30, label: "MAIN ST AT BRIGHTON AVE" },
-      { id: 31, label: "MAIN ST AT CEDARCREST AVE" },
-      { id: 32, label: "MAIN ST AT DELILAH RD" },
-      { id: 33, label: "MAIN ST AT LEEDS AVE" },
-      { id: 34, label: "MAIN ST AT MULBERRY AVE" },
-      { id: 35, label: "MAIN ST AT PLEASANT AVE" },
-      { id: 36, label: "MAIN ST AT READING AVE" },
-      { id: 37, label: "MAIN ST AT REDWOOD AVE" },
-      { id: 38, label: "MAIN ST AT STENTON PL" },
-      { id: 39, label: "MAIN ST AT THOMPSON AVE" },
-      { id: 40, label: "MAIN ST AT WOODLAND AVE" },
-      { id: 41, label: "NEW RD AT DELAWARE AVE" },
-      { id: 42, label: "NEW RD AT MEADOWVIEW AVE" },
-      { id: 43, label: "NEW RD AT NEW JERSEY AVE" },
-      { id: 44, label: "NEW RD AT OHIO AVE" },
-      { id: 45, label: "PITNEY RD AT SPENCER LN" },
-      { id: 46, label: "POMONA RD AT DUERER ST#" },
-      { id: 47, label: "POMONA RD AT JIMMY LEEDS RD" },
-      { id: 48, label: "POMONA RD AT WHITE HORSE PIKE" },
-      { id: 49, label: "PORTER AVE AT ALBANY AVE" },
-      { id: 50, label: "PREFERRED CARE AT ABSECON" },
-      { id: 51, label: "RT-40/322 AT CORDOVA AVE#" },
-      { id: 52, label: "RT-40/322 AT DRESDEN PL" },
-      { id: 53, label: "RT-40/322 AT EAST PLAZA PL#" },
-      { id: 54, label: "RT-40/322 AT FOX PL" },
-      { id: 55, label: "RT-40/322 AT GRANADA AVE" },
-      { id: 56, label: "RT-40/322 AT LYONS COURT" },
-      { id: 57, label: "RT-40/322 AT NAPLES AVE#" },
-      { id: 58, label: "RT-40/322 AT PALERMO AVE#" },
-      { id: 59, label: "RT-40/322 AT PARIS PL#" },
-      { id: 60, label: "RT-40/322 AT RODEWAY INN MOTEL#" },
-      { id: 61, label: "RT-40/322 AT TOULON AVE#" },
-      { id: 62, label: "RT-9 AT OCEAN AVE" },
-      { id: 63, label: "S CAROLINA AVE AT ATLANTIC AVE" },
-      { id: 64, label: "SHORE RD AT BAYVIEW DR" },
-      { id: 65, label: "STOCKTON UNIVERSITY" },
-      { id: 66, label: "US-9 AT CORDERY AVE#" },
-      { id: 67, label: "US-9 AT SHARSWOOD AVE" },
-      { id: 68, label: "W JERSEY AVE AT MAIN ST" },
-      { id: 69, label: "WRANGLEBORO RD AT ALEXANDER DR#" },
-      { id: 70, label: "WRANGLEBORO RD AT COLLINS RD" },
-      { id: 71, label: "WRANGLEBORO RD AT GREAT CREEK RD" },
-      { id: 72, label: "WRANGLEBORO RD AT PEARCE RD#" },
-      { id: 73, label: "WRANGLEBORO RD AT TIMBER GLEN DR CONSUMER SQUARE" },
-      { id: 74, label: "WRANGLEBORO RD AT WALDEN WAY" },
-      { id: 75, label: "WYOMING AVE AT BAYVIEW DR" },
-      { id: 76, label: "WYOMING AVE AT PITNEY RD" },
-    ],
-  };
+  // const bus_stops = {
+  //   508: [
+  //     { id: 1, label: "ACROSS ATLANTIC AVE AT OHIO AVE" },
+  //     { id: 2, label: "ALBANY AVE AT CROSSAN AVE" },
+  //     { id: 3, label: "ALBANY AVE AT FILBERT AVE" },
+  //     { id: 4, label: "ATLANTIC AVE AT ARKANSAS AVE" },
+  //     { id: 5, label: "ATLANTIC AVE AT BOSTON AVE" },
+  //     { id: 6, label: "ATLANTIC AVE AT BRIGHTON AVE" },
+  //     { id: 7, label: "ATLANTIC AVE AT CALIFORNIA AVE" },
+  //     { id: 8, label: "ATLANTIC AVE AT CHELSEA AVE" },
+  //     { id: 9, label: "ATLANTIC AVE AT FLORIDA AVE" },
+  //     { id: 10, label: "ATLANTIC AVE AT KENTUCKY AVE" },
+  //     { id: 11, label: "ATLANTIC AVE AT MARTIN LUTHER KING BLVD" },
+  //     { id: 12, label: "ATLANTIC AVE AT MISSISSIPPI AVE" },
+  //     { id: 13, label: "ATLANTIC AVE AT MONTPELIER AVE" },
+  //     { id: 14, label: "ATLANTIC AVE AT NEW YORK AVE" },
+  //     { id: 15, label: "ATLANTIC CARE ACCESS RD AT HACKBERRY LANE" },
+  //     { id: 16, label: "ATLANTIC COUNTY - SOCIAL SECURITY OFFICE" },
+  //     { id: 17, label: "ATLANTICARE REGIONAL MEDICAL CENTER" },
+  //     { id: 18, label: "CALIFORNIA AVE AT FALCON DR" },
+  //     { id: 19, label: "CALIFORNIA AVE AT NEW RD" },
+  //     { id: 20, label: "COLLINS RD AT FEDERAL CT" },
+  //     { id: 21, label: "COLLINS RD AT GREENWICH DR. EAST" },
+  //     { id: 22, label: "FIRE RD AT CALIFORNIA AVE. (2511 FIRE)#" },
+  //     { id: 23, label: "HAMILTON MALL" },
+  //     { id: 24, label: "JIMMIE LEEDS RD AT ASH AVE" },
+  //     { id: 25, label: "JIMMIE LEEDS RD AT PITNEY RD" },
+  //     { id: 26, label: "JIMMIE LEEDS RD AT PITNEY RD#" },
+  //     { id: 27, label: "JIMMIE LEEDS RD AT SEASHORE GARDENS ENTRANCE" },
+  //     { id: 28, label: "JIMMIE LEEDS RD AT SEAVIEW AVE" },
+  //     { id: 29, label: "MAIN ST AT ADAMS AVE" },
+  //     { id: 30, label: "MAIN ST AT BRIGHTON AVE" },
+  //     { id: 31, label: "MAIN ST AT CEDARCREST AVE" },
+  //     { id: 32, label: "MAIN ST AT DELILAH RD" },
+  //     { id: 33, label: "MAIN ST AT LEEDS AVE" },
+  //     { id: 34, label: "MAIN ST AT MULBERRY AVE" },
+  //     { id: 35, label: "MAIN ST AT PLEASANT AVE" },
+  //     { id: 36, label: "MAIN ST AT READING AVE" },
+  //     { id: 37, label: "MAIN ST AT REDWOOD AVE" },
+  //     { id: 38, label: "MAIN ST AT STENTON PL" },
+  //     { id: 39, label: "MAIN ST AT THOMPSON AVE" },
+  //     { id: 40, label: "MAIN ST AT WOODLAND AVE" },
+  //     { id: 41, label: "NEW RD AT DELAWARE AVE" },
+  //     { id: 42, label: "NEW RD AT MEADOWVIEW AVE" },
+  //     { id: 43, label: "NEW RD AT NEW JERSEY AVE" },
+  //     { id: 44, label: "NEW RD AT OHIO AVE" },
+  //     { id: 45, label: "PITNEY RD AT SPENCER LN" },
+  //     { id: 46, label: "POMONA RD AT DUERER ST#" },
+  //     { id: 47, label: "POMONA RD AT JIMMY LEEDS RD" },
+  //     { id: 48, label: "POMONA RD AT WHITE HORSE PIKE" },
+  //     { id: 49, label: "PORTER AVE AT ALBANY AVE" },
+  //     { id: 50, label: "PREFERRED CARE AT ABSECON" },
+  //     { id: 51, label: "RT-40/322 AT CORDOVA AVE#" },
+  //     { id: 52, label: "RT-40/322 AT DRESDEN PL" },
+  //     { id: 53, label: "RT-40/322 AT EAST PLAZA PL#" },
+  //     { id: 54, label: "RT-40/322 AT FOX PL" },
+  //     { id: 55, label: "RT-40/322 AT GRANADA AVE" },
+  //     { id: 56, label: "RT-40/322 AT LYONS COURT" },
+  //     { id: 57, label: "RT-40/322 AT NAPLES AVE#" },
+  //     { id: 58, label: "RT-40/322 AT PALERMO AVE#" },
+  //     { id: 59, label: "RT-40/322 AT PARIS PL#" },
+  //     { id: 60, label: "RT-40/322 AT RODEWAY INN MOTEL#" },
+  //     { id: 61, label: "RT-40/322 AT TOULON AVE#" },
+  //     { id: 62, label: "RT-9 AT OCEAN AVE" },
+  //     { id: 63, label: "S CAROLINA AVE AT ATLANTIC AVE" },
+  //     { id: 64, label: "SHORE RD AT BAYVIEW DR" },
+  //     { id: 65, label: "STOCKTON UNIVERSITY" },
+  //     { id: 66, label: "US-9 AT CORDERY AVE#" },
+  //     { id: 67, label: "US-9 AT SHARSWOOD AVE" },
+  //     { id: 68, label: "W JERSEY AVE AT MAIN ST" },
+  //     { id: 69, label: "WRANGLEBORO RD AT ALEXANDER DR#" },
+  //     { id: 70, label: "WRANGLEBORO RD AT COLLINS RD" },
+  //     { id: 71, label: "WRANGLEBORO RD AT GREAT CREEK RD" },
+  //     { id: 72, label: "WRANGLEBORO RD AT PEARCE RD#" },
+  //     { id: 73, label: "WRANGLEBORO RD AT TIMBER GLEN DR CONSUMER SQUARE" },
+  //     { id: 74, label: "WRANGLEBORO RD AT WALDEN WAY" },
+  //     { id: 75, label: "WYOMING AVE AT BAYVIEW DR" },
+  //     { id: 76, label: "WYOMING AVE AT PITNEY RD" },
+  //   ],
+  // };
   
   return (
     <div className="App">
--- server/server.js
+++ server/server.js
@@ -2,108 +2,49 @@ const express = require('express');
 const request = require('request');
 const app = express();
 const cors = require("cors");
-const bus_stops= {"508":{
-    "1": "ACROSS ATLANTIC AVE AT OHIO AVE",
-    "2": "ALBANY AVE AT CROSSAN AVE",
-    "3": "ALBANY AVE AT FILBERT AVE",
-    "4": "ATLANTIC AVE AT ARKANSAS AVE",
-    "5": "ATLANTIC AVE AT BOSTON AVE",
-    "6": "ATLANTIC AVE AT BRIGHTON AVE",
-    "7": "ATLANTIC AVE AT CALIFORNIA AVE",
-    "8": "ATLANTIC AVE AT CHELSEA AVE",
-    "9": "ATLANTIC AVE AT FLORIDA AVE",
-    "10": "ATLANTIC AVE AT KENTUCKY AVE",
-    "11": "ATLANTIC AVE AT MARTIN LUTHER KING BLVD",
-    "12": "ATLANTIC AVE AT MISSISSIPPI AVE",
-    "13": "ATLANTIC AVE AT MONTPELIER AVE",
-    "14": "ATLANTIC AVE AT NEW YORK AVE",
-    "15": "ATLANTIC CARE ACCESS RD AT HACKBERRY LANE",
-    "16": "ATLANTIC COUNTY - SOCIAL SECURITY OFFICE",
-    "17": "ATLANTICARE REGIONAL MEDICAL CENTER",
-    "18": "CALIFORNIA AVE AT FALCON DR",
-    "19": "CALIFORNIA AVE AT NEW RD",
-    "20": "COLLINS RD AT FEDERAL CT",
-    "21": "COLLINS RD AT GREENWICH DR. EAST",
-    "22": "FIRE RD AT CALIFORNIA AVE. (2511 FIRE)#",
-    "23": "HAMILTON MALL",
-    "24": "JIMMIE LEEDS RD AT ASH AVE",
-    "25": "JIMMIE LEEDS RD AT PITNEY RD",
-    "26": "JIMMIE LEEDS RD AT PITNEY RD#",
-    "27": "JIMMIE LEEDS RD AT SEASHORE GARDENS ENTRANCE",
-    "28": "JIMMIE LEEDS RD AT SEAVIEW AVE",
-    "29": "MAIN ST AT ADAMS AVE",
-    "30": "MAIN ST AT BRIGHTON AVE",
-    "31": "MAIN ST AT CEDARCREST AVE",
-    "32": "MAIN ST AT DELILAH RD",
-    "33": "MAIN ST AT LEEDS AVE",
-    "34": "MAIN ST AT MULBERRY AVE",
-    "35": "MAIN ST AT PLEASANT AVE",
-    "36": "MAIN ST AT READING AVE",
-    "37": "MAIN ST AT REDWOOD AVE",
-    "38": "MAIN ST AT STENTON PL",
-    "39": "MAIN ST AT THOMPSON AVE",
-    "40": "MAIN ST AT WOODLAND AVE",
-    "41": "NEW RD AT DELAWARE AVE",
-    "42": "NEW RD AT MEADOWVIEW AVE",
-    "43": "NEW RD AT NEW JERSEY AVE",
-    "44": "NEW RD AT OHIO AVE",
-    "45": "PITNEY RD AT SPENCER LN",
-    "46": "POMONA RD AT DUERER ST#",
-    "47": "POMONA RD AT JIMMY LEEDS RD",
-    "48": "POMONA RD AT WHITE HORSE PIKE",
-    "49": "PORTER AVE AT ALBANY AVE",
-    "50": "PREFERRED CARE AT ABSECON",
-    "51": "RT-40/322 AT CORDOVA AVE#",
-    "52": "RT-40/322 AT DRESDEN PL",
-    "53": "RT-40/322 AT EAST PLAZA PL#",
-    "54": "RT-40/322 AT FOX PL",
-    "55": "RT-40/322 AT GRANADA AVE",
-    "56": "RT-40/322 AT LYONS COURT",
-    "57": "RT-40/322 AT NAPLES AVE#",
-    "58": "RT-40/322 AT PALERMO AVE#",
-    "59": "RT-40/322 AT PARIS PL#",
-    "60": "RT-40/322 AT RODEWAY INN MOTEL#",
-    "61": "RT-40/322 AT TOULON AVE#",
-    "62": "RT-9 AT OCEAN AVE",
-    "63": "S CAROLINA AVE AT ATLANTIC AVE",
-    "64": "SHORE RD AT BAYVIEW DR",
-    "65": "STOCKTON UNIVERSITY",
-    "66": "US-9 AT CORDERY AVE#",
-    "67": "US-9 AT SHARSWOOD AVE",
-    "68": "W JERSEY AVE AT MAIN ST",
-    "69": "WRANGLEBORO RD AT ALEXANDER DR#",
-    "70": "WRANGLEBORO RD AT COLLINS RD",
-    "71": "WRANGLEBORO RD AT GREAT CREEK RD",
-    "72": "WRANGLEBORO RD AT PEARCE RD#",
-    "73": "WRANGLEBORO RD AT TIMBER GLEN DR CONSUMER SQUARE",
-    "74": "WRANGLEBORO RD AT WALDEN WAY",
-    "75": "WYOMING AVE AT BAYVIEW DR",
-    "76": "WYOMING AVE AT PITNEY RD"
- }
-
-};
-
-json = JSON.stringify(bus_stops);
-// const response = await openai.chat.completions.create({
-//     model: "gpt-3.5-turbo",
-//     messages: [{"role": "system", "content": "You are a helpful assistant."},
-//         {"role": "user", "content": "Who won the world series in 2020?"},
-//         {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-//         {"role": "user", "content": "Where was it played?"}],
-// });
-// const responseMessage = response.choices[0].message;
-// console.log(responseMessage);
-app.get("/",(req,res)=>{
-    res.send(json)
-})
+const fs = require('fs');
 
 var longitude = -33.8670522;
 var latitude = 151.1957362;
 var radius = 10000;
 
-app
-    .use(express.json())
-    .use(cors({ origin: ['http://localhost:5173'] }));
+app.use(express.json()).use(cors({ origin:['http://localhost:5173']}));
+app.get('/bus_stops', (req, res) => {
+    const busStops = {};
+    const stop_loc = {};
+    fs.readFile('./assets/Bus_Routes.txt', 'utf8', (err, stops) => {
+        if (err) {
+          console.error('Error reading the file:', err);
+          return;
+        }
+        fs.readFile('./assets/bus_stops.txt', 'utf8', (err, loc) => {
+            if (err) {
+              console.error('Error reading the file:', err);
+              return;
+            }
+            const lines = loc.split('\r\n');
+            for (const line of lines) {
+                const [id,stopId,stop_name,stop_desc,stop_lat,stop_lon,zone_id] = line.split(',');
+                stop_loc[stopId] = {'lat': stop_lat, 'long': stop_lon};
+            }
+            
+        
+            const lines1 = stops.split('\r\n');
+            for (const line of lines1) {
+                const [lineId, routeId, direction, municipality, on, at,desc, stopId] = line.split(',');
+                if (direction == 'Inbound'){
+                    if (busStops[lineId]){
+                        busStops[lineId][stopId] = {'name': desc, 'loc': stop_loc[stopId]};
+                    }
+                    else{
+                        busStops[lineId] = {[stopId] : {'name': desc}}
+                    }
+                }
+            }
+            res.json(busStops);
+        });
+    });
+});
 
 app.get("/results", (req, res) => {
     request.get({
@@ -140,7 +81,6 @@ app.get('/autocomplete/:searchQuery', async (req, res) => {
     }
 });
 
-// Assuming you have some port number defined, e.g., 3000
 const PORT = 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
